2dc9e31b42c8d7851d1ac10b9ac413b5970a2825
joycekoh0814@gmail.com <joycekoh0814@gmail.com>
2019-01-07 03:13:46 +0800

    Google Map API (Delivery Boy)

---
+++ FinalG3FoodOrderingSystem/Controllers/gmapController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FinalG3FoodOrderingSystem.Controllers
+{
+    public class gmapController : Controller
+    {
+        // GET: gmap
+        public ActionResult gmap()
+        {
+            return View();
+        }
+    }
+}
